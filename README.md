f9f4552f0da9e6eb5166abe95584d60cc1e0d092
romaincazier <romain@eprc.studio>
2023-03-27 19:02:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TextformatterFootnotes
+
+Description soon
---
+++ TextformatterFootnotes.info.php
@@ -0,0 +1,11 @@
+<?php namespace ProcessWire;
+
+$info = [
+	"title" => "Footnotes",
+	"version" => "0.0.1", 
+	"summary" => "Adds footnotes using Markdown Extra’s syntax, minus Markdown",
+	"author" => "EPRC",
+	"singular" => true,
+	"autoload" => true,
+	"icon" => "superscript",
+];
