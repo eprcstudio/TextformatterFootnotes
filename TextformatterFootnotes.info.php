0560f2142ac48f3d836237d903c391ed40cff6d2
romaincazier <romain@eprc.studio>
2023-03-29 11:12:24 +0200

    Added link to repo

--- TextformatterFootnotes.info.php
+++ TextformatterFootnotes.info.php
@@ -2,9 +2,10 @@
 
 $info = [
 	"title" => "Footnotes",
-	"version" => "0.0.2", 
+	"version" => "0.0.2",
 	"summary" => "Adds footnotes using Markdown Extra’s syntax, minus Markdown",
 	"author" => "EPRC",
+	"href" => "https://github.com/eprcstudio/TextformatterFootnotes",
 	"singular" => true,
 	"autoload" => true,
 	"icon" => "superscript",
