2109202071c3382e5aa7a3737c44ef9613bbcf52
Romain Cazier <romain@eprc.studio>
2024-07-25 13:05:02 +0200

    Bump version

--- TextformatterFootnotes.info.php
+++ TextformatterFootnotes.info.php
@@ -2,7 +2,7 @@
 
 $info = [
 	"title" => "Footnotes",
-	"version" => "0.0.3",
+	"version" => "0.0.4",
 	"summary" => "Adds footnotes using Markdown Extra’s syntax, minus Markdown",
 	"author" => "EPRC",
 	"href" => "https://github.com/eprcstudio/TextformatterFootnotes",
