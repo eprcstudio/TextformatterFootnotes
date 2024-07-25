253ae58c294eccef33a180f7a009ea246c1227e8
Romain Cazier <romain@eprc.studio>
2024-07-25 12:06:38 +0200

    Remove `autoload` and `singular`
    These are not needed for a textformatter

--- TextformatterFootnotes.info.php
+++ TextformatterFootnotes.info.php
@@ -6,7 +6,5 @@ $info = [
 	"summary" => "Adds footnotes using Markdown Extra’s syntax, minus Markdown",
 	"author" => "EPRC",
 	"href" => "https://github.com/eprcstudio/TextformatterFootnotes",
-	"singular" => true,
-	"autoload" => true,
 	"icon" => "superscript",
 ];
